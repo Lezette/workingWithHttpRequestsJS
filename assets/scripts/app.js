0233edbf8e509b1dedd5211121ecdf2c5be5e009
Elizabeth <leezeelola@gmail.com>
2020-06-24 10:26:37 +0100

    delete data

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,7 @@ const listItem = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 const form = document.querySelector("#new-post form");
 const fetchButton = document.querySelector("#available-posts button");
+const postList = document.querySelector("ul");
 
 const sentHttpRequest = (method, url, data) => {
 	const promise = new Promise((resolve, reject) => {
@@ -13,7 +14,14 @@ const sentHttpRequest = (method, url, data) => {
 		xhr.responseType = "json";
 
 		xhr.onload = () => {
-			resolve(xhr.response);
+			if (xhr.status >= 200 && xhr.status < 300) {
+				resolve(xhr.response);
+			} else {
+				reject(new Error("Something went wrong"));
+			}
+		};
+		xhr.onerror = () => {
+			reject(new Error("failed to load http request"));
 		};
 		xhr.send(JSON.stringify(data));
 	});
@@ -21,16 +29,21 @@ const sentHttpRequest = (method, url, data) => {
 };
 
 async function fetchPost() {
-	const responseData = await sentHttpRequest(
-		"GET",
-		"https://jsonplaceholder.typicode.com/posts"
-	);
-	const postList = responseData;
-	for (const post of postList) {
-		const postEl = document.importNode(postTemplate.content, true);
-		postEl.querySelector("h2").textContent = post.title.toUpperCase();
-		postEl.querySelector("p").textContent = post.body;
-		listItem.append(postEl);
+	try {
+		const responseData = await sentHttpRequest(
+			"GET",
+			"https://jsonplaceholder.typicode.com/posts"
+		);
+		const postList = responseData;
+		for (const post of postList) {
+			const postEl = document.importNode(postTemplate.content, true);
+			postEl.querySelector("h2").textContent = post.title.toUpperCase();
+			postEl.querySelector("p").textContent = post.body;
+			postEl.querySelector("li").id = post.id;
+			listItem.append(postEl);
+		}
+	} catch (error) {
+		alert(error.message);
 	}
 }
 
@@ -51,4 +64,14 @@ form.addEventListener("submit", (e) => {
 	const content = e.currentTarget.querySelector("#content").value;
 
 	createPost(title, content);
+});
+
+postList.addEventListener("click", (e) => {
+	if (e.target.tagName === "BUTTON") {
+		const postId = e.target.closest("li").id;
+		sentHttpRequest(
+			"DELETE",
+			`https://jsonplaceholder.typicode.com/posts/${postId}`
+		);
+	}
 });
